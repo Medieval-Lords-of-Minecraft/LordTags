07a90bb00152aba46e49bb82328d9c6df7d71f47
ShanaChans <CedarMid@gmail.com>
2022-12-30 23:07:15 -0700

    Added GUI for tags

--- LordTags/src/me/ShanaChans/LordTags/Inventories/LordTagsInventory.java
+++ LordTags/src/me/ShanaChans/LordTags/Inventories/LordTagsInventory.java
@@ -5,13 +5,16 @@ import java.util.Arrays;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 
 import me.ShanaChans.LordTags.Tag;
 import me.ShanaChans.LordTags.TagManager;
@@ -20,55 +23,184 @@ public class LordTagsInventory extends CustomInventory
 {
 	private final int MENU_MODEL = 5000;
 	private final int TAG = 5001;
+	private final int NEXT = 5002;
+	private final int PREV = 5003;
+	private final int HEAD = 5004;
+	private int page;
 	private Inventory inv;
+	private ArrayList<String> ids;
+	private int tagAmount;
+	private Player p;
 	
 	public LordTagsInventory(Player p, int tagAmount, ArrayList<String> ids) 
 	{
 		inv = Bukkit.createInventory(p, 54, "§4Total Tags: " + tagAmount);
-		ItemStack[] contents = inv.getContents();
-		//ItemStack border = createGuiItem(Material.BLACK_STAINED_GLASS_PANE, " ", " ");
-
-		int i = 0;
-		for(String id : ids)
+		this.p = p;
+		this.ids = ids;
+		this.tagAmount = tagAmount;
+		page = 1;
+		
+		
+		invSetup();
+		setupInventory(p, inv, this);
+	}
+	
+	public void invSetup()
+	{
+		inv.clear();
+		String currentTag = TagManager.getPlayerTag(p);
+		if(currentTag == null || currentTag == "")
 		{
-			if(TagManager.getTags().containsKey(id))
+			currentTag = "§7None";
+		}
+		else
+		{
+			currentTag = TagManager.getTags().get(currentTag).getTagDisplay();
+		}
+		
+		ItemStack[] contents = inv.getContents();
+		ItemStack border = createGuiItem(Material.BLACK_STAINED_GLASS_PANE, MENU_MODEL, " ", " ");
+		ItemStack head = createGuiItem(Material.PLAYER_HEAD, HEAD, "§6Current Tag§7: " + currentTag, "§7Click to remove current tag!");
+		ItemStack next = createGuiItem(Material.GREEN_STAINED_GLASS_PANE, NEXT, "§6Next", "§7Page " + (page + 1));
+		ItemStack prev = createGuiItem(Material.RED_STAINED_GLASS_PANE, PREV, "§6Previous", "§7Page " + (page - 1));
+		SkullMeta playerHead = (SkullMeta) head.getItemMeta();
+		playerHead.setOwningPlayer(p);
+		head.setItemMeta(playerHead);
+		//TagManager.getPlayers().get(p.getUniqueId()).getCurrentTag();
+		
+		contents[0] = border;
+		contents[1] = border;
+		contents[2] = border;
+		contents[3] = border;
+		contents[4] = border;
+		contents[5] = border;
+		contents[6] = border;
+		contents[7] = border;
+		contents[8] = border;
+		
+		contents[9] = border;
+		contents[17] = border;
+		contents[18] = border;
+		contents[26] = border;
+		contents[27] = border;
+		contents[35] = border;
+		contents[36] = border;
+		contents[44] = border;
+		
+		
+		if(page > 1)
+		{
+			contents[45] = prev;
+		}
+		else
+		{
+			contents[45] = border;
+		}
+		
+		contents[46] = border;
+		contents[47] = border;
+		contents[48] = border;
+		contents[49] = head;
+		contents[50] = border;
+		contents[51] = border;
+		contents[52] = border;
+		
+		
+		if(tagAmount > 28 * page)
+		{
+			contents[53] = next;
+		}
+		else
+		{
+			contents[53] = border;
+		}
+		
+		if(!ids.isEmpty())
+		{
+			int i = 0;
+			int j = 0;
+			for(int k = 0 + 28 * (page - 1); k < ids.size(); k++)
 			{
-				Tag tag = TagManager.getTags().get(id);
-				contents[i] = createTagGuiItem(Material.NAME_TAG, "§6Display§7: §f" + tag.getTagDisplay(), "§7" + tag.getTagDesc());
-				i++;
+				
+				if(i >= 4)
+				{
+					continue;
+				}
+				
+				Tag tag = TagManager.getTags().get(ids.get(k));
+				ItemStack item = new ItemStack(Material.NAME_TAG, 1);
+				ItemMeta meta = item.getItemMeta();
+				ArrayList<String> lore = new ArrayList<String>();
+				lore.add("§6Display§7: §f" + tag.getTagDisplay());
+				lore.add("§7" + tag.getTagDesc());
+				//System.out.println(TagManager.getPlayers().get(p.getUniqueId()).getCurrentTag());
+				//.out.println(tag.getTagId());
+				if(TagManager.getPlayers().get(p.getUniqueId()).getCurrentTag().equals(tag.getTagId()))
+				{
+					meta.addEnchant(Enchantment.LUCK, 1, true);
+					meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+				}
+				meta.setLore(lore);
+				meta.setDisplayName("§cID: " + tag.getTagId());
+				meta.setCustomModelData(TAG);
+				item.setItemMeta(meta);
+				
+				contents[(10 + 9*i) + j] = item;
+				
+				
+				if(j >= 6)
+				{
+					i++;
+					j = 0;
+				}
+				else
+				{
+					j++;
+				}
+				
 			}
 		}
 		inv.setContents(contents);
-
-		setupInventory(p, inv, this);
-	}
-
-	protected ItemStack createGuiItem(final Material material, final String name, final String... lore) {
-		final ItemStack item = new ItemStack(material, 1);
-		final ItemMeta meta = item.getItemMeta();
-		meta.setLore(Arrays.asList(lore));
-		meta.setDisplayName(name);
-		meta.setCustomModelData(MENU_MODEL);
-		item.setItemMeta(meta);
-		return item;
 	}
 	
-	protected ItemStack createTagGuiItem(final Material material, final String name, final String... lore) {
+
+	protected ItemStack createGuiItem(final Material material, final int modelID, final String name, final String... lore) {
 		final ItemStack item = new ItemStack(material, 1);
 		final ItemMeta meta = item.getItemMeta();
 		meta.setLore(Arrays.asList(lore));
 		meta.setDisplayName(name);
-		meta.setCustomModelData(TAG);
+		meta.setCustomModelData(modelID);
 		item.setItemMeta(meta);
 		return item;
 	}
 	
 	public void handleInventoryClick(final InventoryClickEvent e) {
-		
+		e.setCancelled(true);
+		if(isNext(e.getCurrentItem()))
+		{
+			page++;
+			invSetup();
+		}
+		if(isPrev(e.getCurrentItem()))
+		{
+			page--;
+			invSetup();
+		}
+		if(isTag(e.getCurrentItem()))
+		{
+			String id = e.getCurrentItem().getItemMeta().getDisplayName().substring(6);
+			TagManager.getPlayers().get(((Player) e.getWhoClicked()).getUniqueId()).setCurrentTag(id);
+			invSetup();
+		}
+		if(isHead(e.getCurrentItem()))
+		{
+			TagManager.getPlayers().get(((Player) e.getWhoClicked()).getUniqueId()).setCurrentTag("");
+			invSetup();
+		}
 	}
 	
 	public void handleInventoryDrag(final InventoryDragEvent e) {
-		
+		e.setCancelled(true);
 	}
 	
 	public void handleInventoryClose(final InventoryCloseEvent e) {
@@ -78,6 +210,26 @@ public class LordTagsInventory extends CustomInventory
 	public boolean isUntouchable(ItemStack item) {
 		return item != null && item.hasItemMeta() && item.getItemMeta().hasCustomModelData() &&
 				(item.getItemMeta().getCustomModelData() == MENU_MODEL || item.getItemMeta().getCustomModelData() == TAG);
+	}
+	
+	public boolean isNext(ItemStack item) {
+		return item != null && item.hasItemMeta() && item.getItemMeta().hasCustomModelData() &&
+				item.getItemMeta().getCustomModelData() == NEXT;
+	}
+	
+	public boolean isPrev(ItemStack item) {
+		return item != null && item.hasItemMeta() && item.getItemMeta().hasCustomModelData() &&
+				item.getItemMeta().getCustomModelData() == PREV;
+	}
+	
+	public boolean isTag(ItemStack item) {
+		return item != null && item.hasItemMeta() && item.getItemMeta().hasCustomModelData() &&
+				item.getItemMeta().getCustomModelData() == TAG;
+	}
+	
+	public boolean isHead(ItemStack item) {
+		return item != null && item.hasItemMeta() && item.getItemMeta().hasCustomModelData() &&
+				item.getItemMeta().getCustomModelData() == HEAD;
 	}
 
 }
--- LordTags/src/me/ShanaChans/LordTags/TagManager.java
+++ LordTags/src/me/ShanaChans/LordTags/TagManager.java
@@ -12,6 +12,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -42,7 +43,36 @@ public class TagManager extends JavaPlugin implements Listener{
 		Bukkit.getServer().getLogger().info("LordTags Enabled");
 		Bukkit.getPluginManager().registerEvents(this, this);
 		initCommands();
-		
+		tags.put("Stitch", new Tag("Stitch", "§9Stitch" , "Donated for this tag!"));
+		tags.put("Test1", new Tag("Test1", "Test1" , "Test1"));
+		tags.put("Test2", new Tag("Test2", "Test1" , "Test1"));
+		tags.put("Test3", new Tag("Test3", "Test1" , "Test1"));
+		tags.put("Test4", new Tag("Test4", "Test1" , "Test1"));
+		tags.put("Test5", new Tag("Test5", "Test1" , "Test1"));
+		tags.put("Test6", new Tag("Test6", "Test1" , "Test1"));
+		tags.put("Test7", new Tag("Test7", "Test1" , "Test1"));
+		tags.put("Test8", new Tag("Test8", "Test1" , "Test1"));
+		tags.put("Test9", new Tag("Test9", "Test1" , "Test1"));
+		tags.put("Test10", new Tag("Test10", "Test1" , "Test1"));
+		tags.put("Test11", new Tag("Test11", "Test1" , "Test1"));
+		tags.put("Test12", new Tag("Test12", "Test1" , "Test1"));
+		tags.put("Test13", new Tag("Test13", "Test1" , "Test1"));
+		tags.put("Test14", new Tag("Test14", "Test1" , "Test1"));
+		tags.put("Test15", new Tag("Test15", "Test1" , "Test1"));
+		tags.put("Test16", new Tag("Test16", "Test1" , "Test1"));
+		tags.put("Test17", new Tag("Test17", "Test1" , "Test1"));
+		tags.put("Test18", new Tag("Test18", "Test1" , "Test1"));
+		tags.put("Test19", new Tag("Test19", "Test1" , "Test1"));
+		tags.put("Test20", new Tag("Test20", "Test1" , "Test1"));
+		tags.put("Test21", new Tag("Test21", "Test1" , "Test1"));
+		tags.put("Test22", new Tag("Test22", "Test1" , "Test1"));
+		tags.put("Test23", new Tag("Test23", "Test1" , "Test1"));
+		tags.put("Test24", new Tag("Test24", "Test1" , "Test1"));
+		tags.put("Test25", new Tag("Test25", "Test1" , "Test1"));
+		tags.put("Test26", new Tag("Test26", "Test1" , "Test1"));
+		tags.put("Test27", new Tag("Test27", "Test1" , "Test1"));
+		tags.put("Test28", new Tag("Test28", "Test1" , "Test1"));
+		tags.put("Test29", new Tag("Test29", "Test1" , "Test1"));
 		inst = this;
 		
 		RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
@@ -82,7 +112,11 @@ public class TagManager extends JavaPlugin implements Listener{
         
         for(Node node : tagPerms)
         {
-        	ids.add(node.getKey().substring(13));
+        	if(tags.containsKey(node.getKey().substring(13)))
+        	{
+        		ids.add(node.getKey().substring(13));
+            	tagAmount++;
+        	}
         }
         
         new LordTagsInventory(p, tagAmount, ids);
@@ -108,6 +142,15 @@ public class TagManager extends JavaPlugin implements Listener{
 	public static HashMap<String, Tag> getTagCreation() {
 		return tagCreation;
 	}
+	
+	@EventHandler
+    public void join(PlayerJoinEvent e) 
+    {
+       if(!players.containsKey(e.getPlayer().getUniqueId()))
+       {
+    	   players.put(e.getPlayer().getUniqueId(), new TagPlayer(""));
+       }
+    }
 	
 	@EventHandler
 	public void onInventoryClick(InventoryClickEvent e) {
